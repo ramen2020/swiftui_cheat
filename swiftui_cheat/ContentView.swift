428bfda3607c8f88a17e78dbbba4cea6c5a5576e
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-05 08:40:56 +0900

    tabviewを作る

--- swiftui_cheat/ContentView.swift
+++ swiftui_cheat/ContentView.swift
@@ -2,9 +2,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+
     var body: some View {
-        Text("Hello,wirld")
-            .padding(.top, 100)
+        TabView(selection: $selection) {
+            Text("page1")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("First")
+                }
+                .tag(1)
+            
+            Text("page2")
+                .tabItem {
+                    Image(systemName: "favorite.fill")
+                    Text("Second")
+                }
+                .tag(2)
+        }
     }
 }
 
