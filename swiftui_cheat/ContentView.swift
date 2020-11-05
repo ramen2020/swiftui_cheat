31f54121b3676ec8947cc3d97b760bd92b003791
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-06 07:44:21 +0900

    tabview増やす

--- swiftui_cheat/ContentView.swift
+++ swiftui_cheat/ContentView.swift
@@ -9,17 +9,31 @@ struct ContentView: View {
         TabView(selection: $selection) {
             Text("page1")
                 .tabItem {
-                    Image(systemName: "person.fill")
+                    Image(systemName: "person.crop.circle.fill")
                     Text("First")
                 }
                 .tag(1)
             
             Text("page2")
                 .tabItem {
-                    Image(systemName: "favorite.fill")
+                    Image(systemName: "bolt.square.fill")
                     Text("Second")
                 }
                 .tag(2)
+            
+            Text("page3")
+                .tabItem {
+                    Image(systemName: "link.circle.fill")
+                    Text("Third")
+                }
+                .tag(3)
+            
+            Text("page4")
+                .tabItem {
+                    Image(systemName: "play.rectangle.fill")
+                    Text("Forth")
+                }
+                .tag(4)
         }
     }
 }
