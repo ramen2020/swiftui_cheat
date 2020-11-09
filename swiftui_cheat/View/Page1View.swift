012c6696c70e8107f81c6bde2c29080fdd9331bc
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-09 22:25:22 +0900

    page1viewを追加

---
+++ swiftui_cheat/View/Page1View.swift
@@ -0,0 +1,20 @@
+//
+//  Page1View.swift
+//  swiftui_cheat
+//
+//  Created by 宮本光直 on 2021/10/02.
+//
+
+import SwiftUI
+
+struct Page1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Page1View()
+    }
+}
