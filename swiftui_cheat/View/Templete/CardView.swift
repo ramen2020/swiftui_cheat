09140aa894eda49b1069a5526ec26eb9cb5b87ad
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-11 08:50:24 +0900

    card作成

--- swiftui_cheat/View/Templete/CardView.swift
+++ swiftui_cheat/View/Templete/CardView.swift
@@ -7,23 +7,52 @@
 
 import SwiftUI
 
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color.red),
+        ]
+    }
+}
+
 struct CardView: View {
+    let scrum = DailyScrum.data[0]
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
             HStack {
-                Text("コーヒー")
-                    .font(.system(size: 18, weight: .semibold, design: .default))
-                Text("For breakfast today, I had some bread and a cup of coffee. I had a hard time deciding whether to drink café au lait or black coffee, so I went with black coffee.")
-                    .font(.system(size: 14, design: .default))
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Attendees"))
+                    .accessibilityValue(Text("\(scrum.attendees.count)"))
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
             }
+            .font(.caption)
         }
-        .frame(width: 300, height: 500, alignment: .center)
-        .background(Color.blue)
+        .frame(width: 200, height: 100)
+        .padding()
+        .foregroundColor(scrum.color)
+        .background(Color.black)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
     static var previews: some View {
         CardView()
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
+
