619f57b8a475c93ac2179ddee1e4c54ef48a0fcd
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-12 07:11:22 +0900

    ListView作成

---
+++ swiftui_cheat/View/Templete/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  swiftui_cheat
+//
+//  Created by 宮本光直 on 2021/10/02.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
