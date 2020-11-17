14c360e72bd29469b51cc4a272b53a7b05de1f0f
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-17 22:00:01 +0900

    add List view

--- swiftui_cheat/View/Templete/List/ListView.swift
+++ swiftui_cheat/View/Templete/List/ListView.swift
@@ -2,7 +2,16 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            List {
+                Text("item 1")
+                Text("item 2")
+                Text("item 3")
+                Text("item 4")
+                Text("item 5")
+                Text("item 6")
+            }
+        }
     }
 }
 
