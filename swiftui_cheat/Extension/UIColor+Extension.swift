874a79017ed0d9f44b0ec9d111f869cb74ba948c
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-28 00:09:01 +0900

    カラーコードで色を指定できるようにする

---
+++ swiftui_cheat/Extension/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension
+//  swiftui_cheat
+//
+
+import Foundation
+import SwiftUI
+
+extension UIColor {
+    
+    // カラーコードでカラーを指定する。
+    // ex) Color(UIColor(hex: "3d5a80"))
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
+}
