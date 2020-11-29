64ffc98fbf9bc1329e6082f4fca2186192ae51c8
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-11-29 21:01:09 +0900

    Colorをextensionし、マテリアルカラーを使えるように追加

--- swiftui_cheat/Extension/UIColor+Extension.swift
+++ swiftui_cheat/Extension/UIColor+Extension.swift
@@ -18,3 +18,32 @@ extension UIColor {
         self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
     }
 }
+    
+extension Color {
+
+    enum AppColor: String {
+        case red = "f44336"
+        case pink = "E91E63"
+        case purple = "9C27B0"
+        case deepPurple = "673AB7"
+        case indigo = "3F51B5"
+        case blue = "2196F3"
+        case lightBlue = "03A9F4"
+        case cyan = "00BCD4"
+        case teal = "009688"
+        case green = "4CAF50"
+        case lightGreen = "8BC34A"
+        case lime = "CDDC39"
+        case yellow = "FFEB3B"
+        case amber = "FFC107"
+        case orange = "FF9800"
+        case deepOrange = "FF5722"
+        case brown = "795548"
+        case grey = "9E9E9E"
+        case blueGrey = "607D8E"
+    }
+    
+    static func materialColor(colorCode :Color.AppColor) -> Color {
+        return Color(UIColor(hex: colorCode.rawValue))
+    }
+}
