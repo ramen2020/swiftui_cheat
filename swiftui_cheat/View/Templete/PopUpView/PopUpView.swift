0780ac42d4591cf9a899f389c5ac5ab98a1446b6
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-03 22:51:54 +0900

    下からトーストを出す

--- swiftui_cheat/View/Templete/PopUpView/PopUpView.swift
+++ swiftui_cheat/View/Templete/PopUpView/PopUpView.swift
@@ -10,6 +10,7 @@ struct PopUpView: View {
     
     @State var isShowPopup = false
     @State var isShowTopToast = false
+    @State var isShowBottomToast = false
 
     var body: some View {
         VStack{
@@ -27,14 +28,27 @@ struct PopUpView: View {
             }) {
                 Text("TopToast")
             }
+            
+            // 下からトースト
+            Button(action: {
+                isShowBottomToast.toggle()
+            }) {
+                Text("BottomToast")
+            }
         }
         .edgesIgnoringSafeArea(.all)
         .navigationBarTitle("PopUpView", displayMode:.large)
-        .popup(isPresented: $isShowPopup, type: .default, closeOnTap: false, closeOnTapOutside: true, backgroundColor: Color.black.opacity(0.5)) {
+        .popup(isPresented: $isShowPopup, type: .default, closeOnTap: false,
+               closeOnTapOutside: true, backgroundColor: Color.black.opacity(0.5)) {
             createPopup()
         }
-        .popup(isPresented: $isShowTopToast, type: .toast, position: .top, closeOnTap: false, closeOnTapOutside: true, backgroundColor: Color.black.opacity(0.5)) {
+        .popup(isPresented: $isShowTopToast, type: .toast, position: .top, closeOnTap: false,
+               closeOnTapOutside: true, backgroundColor: Color.black.opacity(0.5)) {
             createTopToast()
+        }
+        .popup(isPresented: $isShowBottomToast, type: .toast, position: .bottom, closeOnTap: false,
+               closeOnTapOutside: true, backgroundColor: Color.black.opacity(0.5)) {
+            createBottomToast()
         }
     }
     
@@ -77,29 +91,21 @@ struct PopUpView: View {
     func createTopToast() -> some View {
         VStack {
             Spacer(minLength: 20)
-            HStack() {
-                Image(systemName: "arrowshape.turn.up.right.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: ContentMode.fill)
-                    .frame(width: 25, height: 25)
-                    .cornerRadius(10)
-
-                VStack(alignment: .leading, spacing: 2) {
-                    HStack {
-                        Text("お肉が食べたい")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                        Spacer()
-                        Text("20:00")
-                            .font(.system(size: 12))
-                            .foregroundColor(.white)
-                    }
-
-                    Text("今日もお仕事を頑張りました。ので、ご褒美として、美味しいお肉が食べたいです。焼肉がいいです。ついでにレモンサワーもいただければ飛び跳ねます。")
-                        .lineLimit(2)
-                        .font(.system(size: 14))
+            VStack(alignment: .leading, spacing: 2) {
+                HStack {
+                    Text("お肉が食べたい")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text("20:00")
+                        .font(.system(size: 12))
                         .foregroundColor(.white)
                 }
+
+                Text("今日もお仕事を頑張りました。ので、ご褒美として、美味しいお肉が食べたいです。焼肉がいいです。ついでにレモンサワーもいただければ飛び跳ねます。")
+                    .lineLimit(2)
+                    .font(.system(size: 14))
+                    .foregroundColor(.white)
             }
             .padding(15)
         }
@@ -107,7 +113,33 @@ struct PopUpView: View {
         .frame(height: 110)
         .background(Color.materialColor(colorCode: .deepOrange))
         .cornerRadius(30)
-        .offset(y: 100)
+    }
+    
+    // 下からトースト
+    func createBottomToast() -> some View {
+        VStack {
+            VStack(alignment: .leading, spacing: 2) {
+                HStack {
+                    Text("お鍋が食べたい")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Text("20:00")
+                        .font(.system(size: 12))
+                        .foregroundColor(.white)
+                }
+
+                Text("今日もお仕事を頑張りました。ので、ご褒美として、美味しいお肉が食べたいです。焼肉がいいです。ついでにレモンサワーもいただければ飛び跳ねます。")
+                    .lineLimit(2)
+                    .font(.system(size: 14))
+                    .foregroundColor(.white)
+            }
+            .padding(.horizontal, 15)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 110)
+        .background(Color.materialColor(colorCode: .deepPurple))
+        .cornerRadius(30)
     }
 }
 
