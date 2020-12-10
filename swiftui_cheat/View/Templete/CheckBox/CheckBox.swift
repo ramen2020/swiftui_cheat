9f3e2c34bf161875c4434cf2dcfa443956020304
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-11 07:31:22 +0900

    checkbox着手

---
+++ swiftui_cheat/View/Templete/CheckBox/CheckBox.swift
@@ -0,0 +1,18 @@
+//
+//  CheckBox
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct CheckBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckBox_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckBox()
+    }
+}
