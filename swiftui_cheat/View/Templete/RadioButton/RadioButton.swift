341d59d3f772afeeb6a6fe45826872b204b3c443
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-12 08:11:08 +0900

    radiobutton仮作成

---
+++ swiftui_cheat/View/Templete/RadioButton/RadioButton.swift
@@ -0,0 +1,18 @@
+//
+//  RadioButton
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButton()
+    }
+}
