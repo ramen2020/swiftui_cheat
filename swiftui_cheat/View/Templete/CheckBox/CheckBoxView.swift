ea7e5e0a14dba3c08723cfaf63c6fbdfbb899077
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-13 08:03:33 +0900

    checkboxを追加

--- swiftui_cheat/View/Templete/CheckBox/CheckBoxView.swift
+++ swiftui_cheat/View/Templete/CheckBox/CheckBoxView.swift
@@ -6,8 +6,26 @@
 import SwiftUI
 
 struct CheckBoxView: View {
+    
+    @State private var checked = false
+       
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            CheckBox(checked: $checked, onTapped: {
+                self.checked ? print("check！") : print("uncheck")
+            })
+            .padding(.vertical, 12)
+            
+            Text("チェックボックスです。")
+                .font(.system(size: 14, weight: .semibold))
+                .padding(8)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 16)
+        .background(Color(.red))
+        .onTapGesture {
+            checked.toggle() // チェックボックスは小さくて押しにくいから、cellを押した時も反応するようにしている。
+        }
     }
 }
 
