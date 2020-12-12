67ef25b8308b4d6220254f1bfcde0b553d699b96
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-13 07:21:30 +0900

    add checkbox

--- swiftui_cheat/View/Templete/CheckBox/CheckBox.swift
+++ swiftui_cheat/View/Templete/CheckBox/CheckBox.swift
@@ -5,14 +5,37 @@
 
 import SwiftUI
 
+
 struct CheckBox: View {
+    
+    @Binding var checked: Bool
+    
+    init(checked: Binding<Bool>, onTapped: @escaping () -> Void) {
+        self._checked = checked
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            self.$checked.wrappedValue.toggle()
+        }) {
+            if !checked {
+                RoundedRectangle(cornerRadius: 4)
+                    .stroke(Color.gray, lineWidth: 1)
+                    .frame(width: 20, height: 20)
+                    .background(Color.red)
+            } else {
+                Image(systemName: "checkmark.square.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
-struct CheckBox_Previews: PreviewProvider {
-    static var previews: some View {
-        CheckBox()
-    }
-}
+//
+//struct CheckBox_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CheckBox()
+//    }
+//}
