f8f5243eb395fa41b15ae876197ce63ce782792f
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-13 07:24:11 +0900

    add checkbox view

---
+++ swiftui_cheat/View/Templete/CheckBox/CheckBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckBoxView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct CheckBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckBoxView()
+    }
+}
