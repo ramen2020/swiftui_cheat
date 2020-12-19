d536ce0e51a2849d1706b5d8d4c2d175425762ff
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-19 22:13:44 +0900

    radiobutton  縦version 追加

--- swiftui_cheat/View/Templete/RadioButton/RadioButtonView.swift
+++ swiftui_cheat/View/Templete/RadioButton/RadioButtonView.swift
@@ -7,19 +7,29 @@ import SwiftUI
 
 struct RadioButtonView: View {
     
-    @State private var selectedIndex = 0
+    @State private var selectedBreakfastIndex = 0
+    @State private var selectedLanguageIndex = 0
 
-    let breakfastTexts = ["パン", "おにぎり", "食べない", "その他"]
+    let breakfastTexts = ["パン", "おにぎり", "その他"]
+    let languageTexts = ["Flutter", "Swift", "Kotlin", "その他"]
 
     var body: some View {
-        VStack {
-            Text("・ 朝食は何を食べますか？")
-                .frame(maxWidth: .infinity, alignment: .leading)
-            RadioButton(selectedIndex: $selectedIndex, axis: .horizontal, texts: breakfastTexts)
-                .frame(maxWidth: .infinity, alignment: .leading)
+        VStack(spacing: 30) {
+            VStack {
+                Text("・ 朝食は何を食べますか？")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                RadioButton(selectedIndex: $selectedBreakfastIndex, axis: .horizontal, texts: breakfastTexts)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+            VStack {
+                Text("・ 好きな言語は何ですか？")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                RadioButton(selectedIndex: $selectedLanguageIndex, axis: .vertical, texts: languageTexts)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
         }
-        .padding(.horizontal, 10)
-
+        .padding(.horizontal, 20)
     }
 }
 
