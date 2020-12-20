4269d8767b93fa1b88842c0a2d27a1de473346a6
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-21 00:16:58 +0900

    変数から画面遷移できるnavigationlinkを追加

--- swiftui_cheat/View/Templete/NavigationLink/NavigationLinkView.swift
+++ swiftui_cheat/View/Templete/NavigationLink/NavigationLinkView.swift
@@ -6,8 +6,44 @@
 import SwiftUI
 
 struct NavigationLinkView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            NavigationLinkCell(title: "test1", destination: NavigationLink1View())
+            NavigationLinkCell(title: "test2", destination: NavigationLink2View())
+            NavigationLinkCell(title: "test3", destination: NavigationLink3View())
+        }
+        .frame(maxHeight: .infinity)
+    }
+}
+
+struct NavigationLink1View: View {
+    var body: some View {
+        Text("NavigationLink1View")
+    }
+}
+
+struct NavigationLink2View: View {
+    var body: some View {
+        Text("NavigationLink2View")
+    }
+}
+
+struct NavigationLink3View: View {
+    var body: some View {
+        Text("NavigationLink3View")
+    }
+}
+
+struct NavigationLinkCell<Destination : View>: View {
+
+    let title: String
+    let destination: Destination
+    
+    var body: some View {
+        NavigationLink(destination: destination) {
+            Text(title)
+        }
     }
 }
 
