91ca51ea0543ce36120977e884e638eb449c8fbd
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-20 23:23:33 +0900

    navigation link viewを追加

---
+++ swiftui_cheat/View/Templete/NavigationLink/NavigationLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationLinkView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct NavigationLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationLinkView()
+    }
+}
