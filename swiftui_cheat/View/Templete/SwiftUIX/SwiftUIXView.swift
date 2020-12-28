baa00644189b3604e162fe405c97a61de9ad0bd1
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-28 21:30:34 +0900

    add swiftuix view

---
+++ swiftui_cheat/View/Templete/SwiftUIX/SwiftUIXView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIXView
+//  swiftui_cheat
+//
+
+import SwiftUI
+import SwiftUIX
+
+struct SwiftUIXView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIXView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIXView()
+    }
+}
