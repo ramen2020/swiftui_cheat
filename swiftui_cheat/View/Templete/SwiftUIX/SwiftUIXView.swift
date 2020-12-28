24ad8a2b17738b9e1b2d9ebc217654081d13d91a
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-28 21:40:24 +0900

    add swiftuix navigation barに画像を載せる

--- swiftui_cheat/View/Templete/SwiftUIX/SwiftUIXView.swift
+++ swiftui_cheat/View/Templete/SwiftUIX/SwiftUIXView.swift
@@ -9,6 +9,7 @@ import SwiftUIX
 struct SwiftUIXView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitleView(Image(systemName:"house"), displayMode: .inline)
     }
 }
 
