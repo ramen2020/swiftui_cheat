38a39da0966c8e7071c3479159db0a003be07543
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-12-30 23:39:21 +0900

    add indicator

--- swiftui_cheat/View/Templete/SwiftUIX/SwiftUIXView.swift
+++ swiftui_cheat/View/Templete/SwiftUIX/SwiftUIXView.swift
@@ -7,9 +7,12 @@ import SwiftUI
 import SwiftUIX
 
 struct SwiftUIXView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationBarTitleView(Image(systemName:"house"), displayMode: .inline)
+        VStack{
+            ActivityIndicator()
+        }
+        .navigationBarTitleView(Image(systemName:"house"), displayMode: .inline)
     }
 }
 
