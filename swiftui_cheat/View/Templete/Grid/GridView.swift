f3f64f10e3ccabd80388a6c4420a1aab490787a4
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-11 22:21:22 +0900

    add grid view

---
+++ swiftui_cheat/View/Templete/Grid/GridView.swift
@@ -0,0 +1,18 @@
+//
+//  GridView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
