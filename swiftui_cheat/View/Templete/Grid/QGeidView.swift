98fa8c850d8aabd842469452bae9e3c77484c186
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-11 22:23:14 +0900

    add QGrid view

---
+++ swiftui_cheat/View/Templete/Grid/QGeidView.swift
@@ -0,0 +1,18 @@
+//
+//  QGeidView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct QGeidView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QGeidView_Previews: PreviewProvider {
+    static var previews: some View {
+        QGeidView()
+    }
+}
