a143cb72030e68c52c26f1e8a676be8f4766d3ed
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-16 09:01:27 +0900

    progress bar追加

---
+++ swiftui_cheat/View/Templete/ProgressBar/ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar()
+    }
+}
