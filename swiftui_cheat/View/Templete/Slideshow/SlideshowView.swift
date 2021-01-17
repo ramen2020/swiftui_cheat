8930bf56bec0775f325bd32c58176385accee205
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-18 08:14:24 +0900

    slideshowを作成

---
+++ swiftui_cheat/View/Templete/Slideshow/SlideshowView.swift
@@ -0,0 +1,18 @@
+//
+//  SlideshowView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct SlideshowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SlideshowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SlideshowView()
+    }
+}
