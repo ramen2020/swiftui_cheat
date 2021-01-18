eed61cef431fae0b14dd7e6c062656646b93708f
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-19 08:13:54 +0900

    lottieファイル準備

---
+++ swiftui_cheat/View/Templete/Lottie/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView()
+    }
+}
