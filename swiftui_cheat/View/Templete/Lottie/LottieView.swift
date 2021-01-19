afd7482d1cec59ee6857d47dd3bf2067bae6bde0
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-19 23:01:23 +0900

    波紋の実装

--- swiftui_cheat/View/Templete/Lottie/LottieView.swift
+++ swiftui_cheat/View/Templete/Lottie/LottieView.swift
@@ -4,15 +4,46 @@
 //
 
 import SwiftUI
+import Lottie
 
-struct LottieView: View {
+struct LottiePlacticeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LottieView(name: "voice_wave", loopMode: .loop)
+                .frame(width: 250, height: 250)
+        }
     }
 }
 
-struct LottieView_Previews: PreviewProvider {
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .playOnce
+    
+    var animationView = AnimationView()
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {}
+}
+struct LottiePlacticeView_Previews: PreviewProvider {
     static var previews: some View {
-        LottieView()
+        LottiePlacticeView()
     }
 }
