5a2a90a48c98524659a8747243621e832263ee90
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-24 00:08:10 +0900

    swiftui チュートリアルを行うファイルを追加

---
+++ swiftui_cheat/View/SwiftUITutorial/SwiftUITutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUITutorialView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct SwiftUITutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUITutorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUITutorialView()
+    }
+}
