fd1bbc5e08896b45847de7ab59abce58aa60d3cc
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-24 00:11:43 +0900

    swiftui チュートリアル HandlingUserInput開始

---
+++ swiftui_cheat/View/SwiftUITutorial/HandlingUserInputView.swift
@@ -0,0 +1,18 @@
+//
+//  HandlingUserInputView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct HandlingUserInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HandlingUserInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        HandlingUserInputView()
+    }
+}
