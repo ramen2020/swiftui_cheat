e978ca3b76b668b8017957764529d5272911d3e5
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-01-24 00:11:43 +0900

    swiftui チュートリアル HandlingUserInput遷移できるように修正

--- swiftui_cheat/View/SwiftUITutorial/SwiftUITutorialView.swift
+++ swiftui_cheat/View/SwiftUITutorial/SwiftUITutorialView.swift
@@ -7,7 +7,31 @@ import SwiftUI
 
 struct SwiftUITutorialView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            NavigationLink(destination: HandlingUserInputView()) {
+                Text("HandlingUserInputView")
+            }
+            
+//            NavigationLink(destination: SwiftUIXView()) {
+//                Text("SwiftUIXView")
+//            }
+//            
+//            NavigationLink(destination: QGridView()) {
+//                Text("QGridView")
+//            }
+//
+//            NavigationLink(destination: LottiePlacticeView()) {
+//                Text("LottiePlacticeView")
+//            }
+
+//            NavigationLink(destination: ListView()) {
+//                Text("List")
+//            }
+        }
+        .font(.system(size: 18, weight: .bold))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .foregroundColor(Color.materialColor(colorCode: .amber))
+        .background(Color.materialColor(colorCode: .blue))
     }
 }
 
