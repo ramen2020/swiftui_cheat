13f54b9f9adfebdf925cc5e8334ffd18ca49e541
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-02-04 00:08:53 +0900

    WebViewへ遷移できるように修正

--- swiftui_cheat/View/Templete/WebView/WebView.swift
+++ swiftui_cheat/View/Templete/WebView/WebView.swift
@@ -7,7 +7,9 @@ import SwiftUI
 
 struct WebView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: CustomWKWebView(urlString: "https://www.apple.com/jp/")) {
+            Text("CustomWKWebView")
+        }
     }
 }
 
