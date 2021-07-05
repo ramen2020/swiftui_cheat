64de838c9b5a7ae548839f903835d9d6e6c18e6b
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-05 23:59:11 +0900

    cardを作成

--- swiftui_cheat/View/Templete/Card/CardView.swift
+++ swiftui_cheat/View/Templete/Card/CardView.swift
@@ -18,37 +18,69 @@ extension DailyScrum {
 struct CardView: View {
     let scrum = DailyScrum.data[0]
     var body: some View {
-        VStack(spacing: 30) {
-            VStack(alignment: .leading) {
-                Text(scrum.title)
-                    .font(.headline)
-                Spacer()
-                HStack {
-                    Label("\(scrum.attendees.count)", systemImage: "person.3")
-                        .accessibilityElement(children: .ignore)
-                        .accessibilityLabel(Text("Attendees"))
-                        .accessibilityValue(Text("\(scrum.attendees.count)"))
+        ScrollView {
+            VStack(spacing: 30) {
+                VStack(alignment: .leading) {
+                    Text(scrum.title)
+                        .font(.headline)
                     Spacer()
-                    Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                        .padding(.trailing, 20)
+                    HStack {
+                        Label("\(scrum.attendees.count)", systemImage: "person.3")
+                            .accessibilityElement(children: .ignore)
+                            .accessibilityLabel(Text("Attendees"))
+                            .accessibilityValue(Text("\(scrum.attendees.count)"))
+                        Spacer()
+                        Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                            .padding(.trailing, 20)
+                    }
+                    .font(.caption)
                 }
-                .font(.caption)
+                .frame(width: 200, height: 100)
+                .padding()
+                .foregroundColor(scrum.color)
+                .background(Color.black)
+                
+                // spacing:0で左に詰める。
+                HStack(spacing: 0) {
+                    Image(systemName: "airplane.circle")
+                    Text("機内モード")
+                        .font(.system(size: 12))
+                }
+                .foregroundColor(Color.red)
+                .frame(width: 200, height: 100, alignment: .leading)
+                .background(Color.black)
             }
-            .frame(width: 200, height: 100)
-            .padding()
-            .foregroundColor(scrum.color)
-            .background(Color.black)
             
-            // spacing:0で左に詰める。
-            HStack(spacing: 0) {
-                Image(systemName: "airplane.circle")
-                Text("機内モード")
-                    .font(.system(size: 12))
+            VStack(spacing: 0) {
+                ZStack {}
+                .background(Color.blue)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+                VStack(spacing: 8) {
+                    Text("こんにちわ")
+                        .font(.system(size: 14, weight: .semibold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    Text("おやすみ")
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(Color.white)
+                        .frame(width: 120, height: 30)
+                        .background(Color.pink)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .cornerRadius(6)
+
+                }
+                .foregroundColor(Color.black)
+                .padding(10)
+                .background(Color.white)
             }
-            .foregroundColor(Color.red)
-            .frame(width: 200, height: 100, alignment: .leading)
-            .background(Color.black)
+            .frame(width: 200, height: 170)
+            .background(Color.white)
+            .clipped()
+            .cornerRadius(6)
+            .shadow(color: Color.gray, radius: 5)
         }
+        .frame(maxWidth: .infinity, alignment: .center)
         .navigationBarTitle("Card", displayMode:.large)
     }
 }
