c422d5d9845c3c083483fe7b2b57db2cee8f9d07
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-10 08:02:11 +0900

    星を置く

--- swiftui_cheat/View/Templete/Card/CardView.swift
+++ swiftui_cheat/View/Templete/Card/CardView.swift
@@ -8,7 +8,7 @@ struct CardView: View {
             Card1()
         }
         .frame(maxWidth: .infinity, alignment: .center)
-        .navigationBarTitle("Card", displayMode:.large)
+        .navigationBarTitle("Card", displayMode: .large)
     }
 }
 
@@ -67,43 +67,51 @@ struct CardTurtorial1: View {
 struct Card1: View {
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                ZStack {
-                    Image(systemName: "star.fill")
-                        .resizable()
-                        .frame(width: 25, height: 25)
-                        .foregroundColor(Color.yellow)
+        VStack(spacing: 0) {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        //
+                    }, label: {
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color.yellow)
+                    }).padding([.trailing, .top], 10)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.blue)
-                
-                VStack(spacing: 8) {
-                    Text("こんにちわ")
-                        .font(.system(size: 14, weight: .semibold))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text("おやすみ")
-                        .font(.system(size: 14, weight: .semibold))
-                        .foregroundColor(Color.white)
-                        .frame(width: 120, height: 30)
-                        .background(Color.pink)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .cornerRadius(6)
-                    
-                }
-                .foregroundColor(Color.black)
-                .padding(10)
-                .background(Color.white)
+                Spacer()
             }
-            .frame(width: 200, height: 170)
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.blue)
+            
+            VStack(spacing: 8) {
+                Text("こんにちわ")
+                    .font(.system(size: 15, weight: .semibold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("おやすみ")
+                    .font(.system(size: 12, weight: .semibold))
+                    .foregroundColor(Color.white)
+                    .frame(width: 120, height: 30)
+                    .background(Color.pink)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .cornerRadius(6)
+                
+            }
+            .foregroundColor(Color.black)
+            .padding(10)
             .background(Color.white)
-            .clipped()
-            .cornerRadius(6)
-            .shadow(color: Color.gray, radius: 5)
         }
+        .frame(width: 200, height: 170)
+        .background(Color.white)
+        .clipped()
+        .cornerRadius(6)
+        .shadow(color: Color.gray, radius: 5)
     }
 }
+
 
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.data[0]
