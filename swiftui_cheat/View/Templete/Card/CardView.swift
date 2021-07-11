6334f8bb902e3749e4105995d75e6e54d8c8bfdc
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-12 08:40:08 +0900

    card2を追加

--- swiftui_cheat/View/Templete/Card/CardView.swift
+++ swiftui_cheat/View/Templete/Card/CardView.swift
@@ -5,7 +5,7 @@ struct CardView: View {
     var body: some View {
         ScrollView {
             CardTurtorial1()
-            Card1()
+            Card2()
         }
         .frame(maxWidth: .infinity, alignment: .center)
         .navigationBarTitle("Card", displayMode: .large)
@@ -65,6 +65,54 @@ struct CardTurtorial1: View {
 }
 
 struct Card1: View {
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        //
+                    }, label: {
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color.yellow)
+                    }).padding([.trailing, .top], 10)
+                }
+                Spacer()
+            }
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.blue)
+            
+            VStack(spacing: 8) {
+                Text("こんにちわ")
+                    .font(.system(size: 15, weight: .semibold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("おやすみ")
+                    .font(.system(size: 12, weight: .semibold))
+                    .foregroundColor(Color.white)
+                    .frame(width: 120, height: 30)
+                    .background(Color.pink)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .cornerRadius(6)
+                
+            }
+            .foregroundColor(Color.black)
+            .padding(10)
+            .background(Color.white)
+        }
+        .frame(width: 200, height: 170)
+        .background(Color.white)
+        .clipped()
+        .cornerRadius(6)
+        .shadow(color: Color.gray, radius: 5)
+    }
+}
+
+struct Card2: View {
     
     var body: some View {
         VStack(spacing: 0) {
