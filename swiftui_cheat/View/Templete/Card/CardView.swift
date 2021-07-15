4d8948d51d6bd21a78f91901e78c40449d098e3e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-15 22:10:24 +0900

    cardの追加

--- swiftui_cheat/View/Templete/Card/CardView.swift
+++ swiftui_cheat/View/Templete/Card/CardView.swift
@@ -8,10 +8,11 @@ struct CardView: View {
                 Spacer().frame(height: 30)
                 CardTurtorial1()
                 CardTurtorial2()
+                Card1()
                 Card2()
             }
             .frame(maxWidth: .infinity)
-
+            
         }
         .frame(maxWidth: .infinity, alignment: .center)
         .navigationBarTitle("Card", displayMode: .large)
@@ -64,7 +65,7 @@ struct CardTurtorial1: View {
 }
 
 struct CardTurtorial2: View {
-
+    
     var body: some View {
         VStack(spacing: 30) {
             HStack(spacing: 0) {
@@ -132,48 +133,50 @@ struct Card1: View {
 struct Card2: View {
     
     var body: some View {
-        VStack(spacing: 0) {
-            VStack {
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        //
-                    }, label: {
-                        Image(systemName: "star.fill")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .foregroundColor(Color.yellow)
-                    }).padding([.trailing, .top], 10)
-                }
-                Spacer()
-            }
+        HStack(alignment: .top) {
+            Image("chincoteague")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 150, height: 150)
             
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.blue)
-            
-            VStack(spacing: 8) {
-                Text("こんにちわ")
-                    .font(.system(size: 15, weight: .semibold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Text("おやすみ")
-                    .font(.system(size: 12, weight: .semibold))
+            VStack(alignment: .leading) {
+                Text("タイトル１")
+                    .font(.system(size: 12))
                     .foregroundColor(Color.white)
-                    .frame(width: 120, height: 30)
+                    .padding(.all, 4)
                     .background(Color.pink)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .cornerRadius(6)
+                    .padding(.trailing, 8)
                 
+                Text("タイトル２")
+                    .font(.system(size: 14, weight: .semibold))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.trailing, 8)
+                
+                Text("タイトル３")
+                    .font(.system(size: 14, weight: .semibold))
+                
+                HStack(spacing: 0) {
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color.yellow)
+                    Text("タイトル４")
+                        .font(.system(size: 12))
+                        .padding(.leading, 2)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .foregroundColor(Color.black)
-            .padding(10)
-            .background(Color.white)
+            .padding(.top, 10)
         }
-        .frame(width: 200, height: 170)
+        .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color.white)
-        .clipped()
-        .cornerRadius(6)
-        .shadow(color: Color.gray, radius: 5)
+        .cornerRadius(5)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .shadow(color: Color.gray, radius: 10, x: 0, y: 0)
+        )
+        .padding(20)
     }
 }
 
