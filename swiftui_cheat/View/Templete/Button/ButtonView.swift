460e1c187dcc32fa3d1840a1887a20dfe4df9a8a
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-16 21:09:58 +0900

    add custom button view

---
+++ swiftui_cheat/View/Templete/Button/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView()
+    }
+}
