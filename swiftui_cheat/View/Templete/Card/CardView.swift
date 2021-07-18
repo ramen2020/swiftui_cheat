81ff41c315276e3ffb4c4fad1ab0106dd490b30c
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-19 00:10:02 +0900

    add Turtorial1 file

--- swiftui_cheat/View/Templete/Card/CardView.swift
+++ swiftui_cheat/View/Templete/Card/CardView.swift
@@ -19,51 +19,6 @@ struct CardView: View {
     }
 }
 
-struct DailyScrum {
-    var title: String
-    var attendees: [String]
-    var lengthInMinutes: Int
-    var color: Color
-}
-
-extension DailyScrum {
-    static var data: [DailyScrum] {
-        [
-            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color.red),
-        ]
-    }
-}
-
-
-struct CardTurtorial1: View {
-    let scrum = DailyScrum.data[0]
-    var body: some View {
-        VStack(spacing: 30) {
-            VStack(alignment: .leading) {
-                Text(scrum.title)
-                    .font(.headline)
-                Spacer()
-                HStack {
-                    Label("\(scrum.attendees.count)", systemImage: "person.3")
-                        .accessibilityElement(children: .ignore)
-                        .accessibilityLabel(Text("Attendees"))
-                        .accessibilityValue(Text("\(scrum.attendees.count)"))
-                    Spacer()
-                    Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                        .padding(.trailing, 20)
-                }
-                .font(.caption)
-            }
-            .frame(width: 200, height: 100)
-            .padding()
-            .foregroundColor(scrum.color)
-            .background(Color.black)
-            .cornerRadius(6)
-            .shadow(color: Color.gray, radius: 5)
-        }
-    }
-}
-
 struct CardTurtorial2: View {
     
     var body: some View {
