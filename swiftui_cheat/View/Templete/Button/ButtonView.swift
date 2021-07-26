90201c953e0828c6c4f4d8331134a43daeda369e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-26 20:55:01 +0900

    add custom button1

--- swiftui_cheat/View/Templete/Button/ButtonView.swift
+++ swiftui_cheat/View/Templete/Button/ButtonView.swift
@@ -10,23 +10,29 @@ struct ButtonView: View {
         ScrollView {
             VStack(spacing: 30) {
                 Spacer().frame(height: 30)
-                Button(action: {
-                    
-                }, label: {
-                    Text("すべて")
-                        .foregroundColor(Color.white)
-                        .font(.system(size: 15, weight: .semibold))
-                        .padding(EdgeInsets(top: 8, leading: 15, bottom: 8, trailing: 15))
-                        .background(Color.pink)
-                        .cornerRadius(4)
-                    }
-                )
+                CustomButton1()
                 Spacer().frame(height: 30)
             }
             .frame(maxWidth: .infinity)
         }
         .frame(maxWidth: .infinity, alignment: .center)
         .navigationBarTitle("Button", displayMode: .large)
+    }
+}
+
+struct CustomButton1: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("すべて")
+                .foregroundColor(Color.white)
+                .font(.system(size: 15, weight: .semibold))
+                .padding(EdgeInsets(top: 8, leading: 15, bottom: 8, trailing: 15))
+                .background(Color.pink)
+                .cornerRadius(4)
+            }
+        )
     }
 }
 
