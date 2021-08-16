582503dc9ae2c0fa6e5bd5d55a1f819241e57897
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-16 22:05:23 +0900

    add view extension file

---
+++ swiftui_cheat/Extension/View+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extension
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct View_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View_Extension_Previews: PreviewProvider {
+    static var previews: some View {
+        View_Extension()
+    }
+}
