c5dd82bb8a1d3ad165194050e1ee36f86d0c995b
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-16 22:42:02 +0900

    add black mask on view extension

--- swiftui_cheat/Extension/View+Extension.swift
+++ swiftui_cheat/Extension/View+Extension.swift
@@ -5,14 +5,28 @@
 
 import SwiftUI
 
-struct View_Extension: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct View_Extension_Previews: PreviewProvider {
-    static var previews: some View {
-        View_Extension()
+// black Mask
+extension View {
+    @ViewBuilder
+    func onMask(isMask: Bool, text: String = "") -> some View {
+        if isMask {
+            self
+                .overlay(
+                    GeometryReader { geo in
+                        ZStack(alignment: .leading) {
+                            Rectangle()
+                                .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.5))
+                                .frame(width: geo.size.width)
+                                .cornerRadius(4)
+                            Text(text)
+                                .font(.system(size: 14, weight: .bold))
+                                .foregroundColor(Color.white)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                    }
+                )
+        } else {
+            self
+        }
     }
 }
