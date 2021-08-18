34b2fce54a345a068fece4d4d3b83102ad360325
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-19 07:42:01 +0900

    add skelton view until loding

--- swiftui_cheat/View/CustomView.swift
+++ swiftui_cheat/View/CustomView.swift
@@ -8,11 +8,11 @@ import SwiftUI
 struct CustomView: View {
     var body: some View {
         VStack(spacing: 30) {
-
+            
             NavigationLink(destination: ButtonView()) {
                 Text("ButtonView")
             }
-
+            
             NavigationLink(destination: CustomNavigationBarView()) {
                 Text("CustomNavigationBarView")
             }
@@ -20,11 +20,34 @@ struct CustomView: View {
             NavigationLink(destination: CustomNavigationBarImageView()) {
                 Text("CustomNavigationBarImageView")
             }
+            
+            NavigationLink(destination: SkeletonView()) {
+                Text("SkeletonView")
+            }
         }
         .font(.system(size: 18, weight: .bold))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .foregroundColor(Color.materialColor(colorCode: .amber))
         .background(Color.materialColor(colorCode: .teal))
+    }
+}
+
+struct SkeletonView: View {
+    @State var items = [String]()
+    
+    var body: some View {
+        VStack {
+            Text("こんにちわ")
+                .redacted(reason: .placeholder)
+
+            Text("こんにちわ")
+                .onSkeleton(active: self.items.isEmpty)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.items = ["こんばんわ", "やっほ", "おはよう", "こんばんわ"]
+            }
+        }
     }
 }
 
