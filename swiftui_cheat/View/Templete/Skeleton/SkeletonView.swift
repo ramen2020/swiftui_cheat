98a5b7a66bf643d8303c296ad24abda8fd3efd6a
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-19 07:55:21 +0900

    add skelton view dir

---
+++ swiftui_cheat/View/Templete/Skeleton/SkeletonView.swift
@@ -0,0 +1,18 @@
+//
+//  SkeletonView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct SkeletonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SkeletonView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkeletonView()
+    }
+}
