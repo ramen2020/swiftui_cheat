3b3775fefdd2c74e6c13d80e9d0303abe7388f5f
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-19 07:55:21 +0900

    add skelton view dir

--- swiftui_cheat/View/CustomView.swift
+++ swiftui_cheat/View/CustomView.swift
@@ -32,25 +32,6 @@ struct CustomView: View {
     }
 }
 
-struct SkeletonView: View {
-    @State var items = [String]()
-    
-    var body: some View {
-        VStack {
-            Text("こんにちわ")
-                .redacted(reason: .placeholder)
-
-            Text("こんにちわ")
-                .onSkeleton(active: self.items.isEmpty)
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                self.items = ["こんばんわ", "やっほ", "おはよう", "こんばんわ"]
-            }
-        }
-    }
-}
-
 struct CustomView_Previews: PreviewProvider {
     static var previews: some View {
         CustomView()
--- swiftui_cheat/View/Templete/Skeleton/SkeletonView.swift
+++ swiftui_cheat/View/Templete/Skeleton/SkeletonView.swift
@@ -6,11 +6,23 @@
 import SwiftUI
 
 struct SkeletonView: View {
+    @State var items = [String]()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("こんにちわ")
+                .redacted(reason: .placeholder)
+
+            Text("こんにちわ")
+                .onSkeleton(active: self.items.isEmpty)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.items = ["こんばんわ", "やっほ", "おはよう", "こんばんわ"]
+            }
+        }
     }
 }
-
 struct SkeletonView_Previews: PreviewProvider {
     static var previews: some View {
         SkeletonView()
