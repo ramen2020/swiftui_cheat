c79232661afb70cc92feec0509dc0139cf0f99f6
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-21 23:24:05 +0900

    add slideshow image

--- swiftui_cheat/View/Templete/Slideshow/SlideShowView.swift
+++ swiftui_cheat/View/Templete/Slideshow/SlideShowView.swift
@@ -21,9 +21,51 @@ struct SlideShowView: View {
                 .background(Color.materialColor(colorCode: .orange))
                 .currentPageIndicatorTintColor(.primary)
             }
+
+            SlideShowImage()
+            
+            Spacer().frame(height: 50)
         }
     }
 }
+
+
+struct SlideShowImage: View {
+    
+    @State private var selection = 0
+    
+    var body: some View {
+        VStack(spacing: 0){
+            TabView(selection: $selection) {
+                ForEach(Range(0...4)) { index in
+                    Image("ramen7")
+                        .resizable()
+                        .scaledToFit()
+                        .tag(index)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .frame(height: 300)
+            .frame(maxWidth: .infinity)
+
+            HStack {
+                ForEach(0..<4) { num in
+                    Circle()
+                        .fill(num == selection ? Color.green : Color.gray)
+                        .frame(width: 8, height: 8)
+                        .id(num)
+                        .tag(num)
+                        .onTapGesture(perform: {
+                            withAnimation {
+                                selection = num
+                            }
+                        })
+                }
+            }.frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+}
+
 
 struct SlideShowView_Previews: PreviewProvider {
     static var previews: some View {
