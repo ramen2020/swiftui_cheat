99f4fda523ae0e38da01851b81191a3371ce70ea
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-23 21:28:39 +0900

    add custom webview file

---
+++ swiftui_cheat/View/Templete/WebView/WebView2.swift
@@ -0,0 +1,18 @@
+//
+//  WebView2
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct WebView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView2_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView2()
+    }
+}
