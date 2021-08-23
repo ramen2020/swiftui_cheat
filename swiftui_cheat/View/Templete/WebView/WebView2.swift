eaf7e5d2bc966cd058d22d4f73137e54c3453ec7
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-23 21:32:28 +0900

    add custom webview

--- swiftui_cheat/View/Templete/WebView/WebView2.swift
+++ swiftui_cheat/View/Templete/WebView/WebView2.swift
@@ -1,18 +1,115 @@
 //
-//  WebView2
+//  CustomWebView
 //  swiftui_cheat
 //
 
 import SwiftUI
+import WebKit
 
-struct WebView2: View {
+struct CustomWebView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CustomWebView2(urlString: "https://medium.com/", navigationTitle: "CustomWebView")
     }
 }
 
-struct WebView2_Previews: PreviewProvider {
-    static var previews: some View {
-        WebView2()
+struct CustomWebView2: View {
+
+    private var webView: WebView2
+    var navigationTitle: String
+    var urlString: String
+
+    init(urlString: String, navigationTitle: String) {
+        self.urlString = urlString
+        self.webView = WebView2(urlString: urlString)
+        self.navigationTitle = navigationTitle
+    }
+
+    var body: some View {
+        VStack() {
+            CustomNavigationBar(title: navigationTitle, isReturn: false)
+
+            webView
+
+            HStack(alignment: .top) {
+                Button(action: {
+                    webView.backPage()
+                }) {
+                    Image(systemName: "chevron.backward")
+                        .font(.title3)
+                        .padding(20)
+                }
+
+                Button(action: {
+                    webView.nextPage()
+                }) {
+                    Image(systemName: "chevron.forward")
+                        .font(.title3)
+                        .padding(20)
+                }
+                
+                Spacer()
+            }
+        }
+        .navigationTitle("")
+        .navigationBarHidden(true)
+    }
+}
+
+struct WebView2: UIViewRepresentable {
+    var webView = WKWebView()
+    var urlString: String
+
+    class Coordinator: NSObject, WKUIDelegate, WKNavigationDelegate {
+        var parent: WebView2
+
+        init(_ parent: WebView2) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+            if navigationAction.targetFrame == nil {
+                webView.load(navigationAction.request)
+            }
+            return nil
+        }
+
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+            if let url = navigationAction.request.url?.absoluteString {
+                if (url.hasPrefix("http")) {
+                    decisionHandler(WKNavigationActionPolicy.allow)
+                } else {
+                    decisionHandler(WKNavigationActionPolicy.cancel)
+                }
+            }
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
+        webView.allowsBackForwardNavigationGestures = true
+
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+
+    func backPage() {
+        webView.goBack()
+    }
+
+    func nextPage() {
+        webView.goForward()
     }
 }
