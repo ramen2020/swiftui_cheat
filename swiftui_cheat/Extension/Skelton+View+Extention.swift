a9dbb74abe350485e272a7f4bcf95a16a6f13445
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-24 20:54:02 +0900

    add skelton

---
+++ swiftui_cheat/Extension/Skelton+View+Extention.swift
@@ -0,0 +1,18 @@
+//
+//  Skelton+View+Extention
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct Skelton_View_Extention: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Skelton_View_Extention_Previews: PreviewProvider {
+    static var previews: some View {
+        Skelton_View_Extention()
+    }
+}
