84a1150351dfd77aac9f76ad2344d2db15a68a55
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-25 22:45:20 +0900

    fix list in Skelton View

--- swiftui_cheat/View/Templete/Skeleton/SkeletonView.swift
+++ swiftui_cheat/View/Templete/Skeleton/SkeletonView.swift
@@ -9,28 +9,46 @@ struct SkeletonView: View {
     @State var items = [String]()
     
     var body: some View {
-        VStack {
-            Text("こんにちわ")
-                .redacted(reason: .placeholder)
+        List {
+            Section(header: Text("normal redacted")) {
+                Text("こんにちわ")
+                    .redacted(reason: .placeholder)
+            }
 
-            Text("こんにちわ")
-                .onSkeleton(active: self.items.isEmpty)
+            Section(header: Text("onSkeleton")) {
+                Text("こんにちわ")
+                    .onSkeleton(active: self.items.isEmpty)
+            }
             
-            Card1()
-                .onSkeleton2(active: self.items.isEmpty, type: .blurred)
-            
-            Card1()
-                .onSkeleton2(active: self.items.isEmpty, type: .confidential)
-            
-            Card1()
-                .onSkeleton2(active: self.items.isEmpty, type: .placeholder)
-
-            VStack {
+            Section(header: Text("onSkeleton2 blurred")) {
                 Card1()
-            }.onSkeleton(active: self.items.isEmpty)
+                    .onSkeleton2(active: self.items.isEmpty, type: .blurred)
+            }
+            
+            Section(header: Text("onSkeleton2 confidential")) {
+                Card1()
+                    .onSkeleton2(active: self.items.isEmpty, type: .confidential)
+            }
+            
+            Section(header: Text("onSkeleton2 placeholder")) {
+                Card1()
+                    .onSkeleton2(active: self.items.isEmpty, type: .placeholder)
+            }
+
+            Section(header: Text("normal redacted")) {
+                ScrollView(.horizontal){
+                    HStack(spacing: 10) {
+                        Card1()
+                        Card1()
+                        Card1()
+                        Card1()
+                    }
+                }
+                .onSkeleton(active: self.items.isEmpty)
+            }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
                 self.items = ["こんばんわ", "やっほ", "おはよう", "こんばんわ"]
             }
         }
