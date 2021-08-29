5f457c629bbe5e1c279867a9fe8e9fe61b814343
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-29 23:34:04 +0900

    add List ProgressBar

--- swiftui_cheat/View/Templete/ProgressBar/ProgressBar.swift
+++ swiftui_cheat/View/Templete/ProgressBar/ProgressBar.swift
@@ -9,58 +9,69 @@ struct ProgressBarView: View {
     
     var currentValue: Int = 38
     var maxValue: Int = 100
-
+    
     var body: some View {
         
-        VStack(spacing: 20) {
+        List {
             // 赤 動的
-            Text("現在は、\(Int(currentValue) * 100 / Int(maxValue))% です。")
-            
-            GeometryReader { geometryReader in
-                ZStack(alignment: .leading) {
-                    Capsule()
-                        .frame(height: 7)
-                        .foregroundColor(Color(UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)))
-
-                    Capsule()
-                        .frame(width: self.progress(value: Double(currentValue),
-                                                    maxValue: Double(maxValue),
-                                                    width: geometryReader.size.width),
-                               height: 7)
-                        .foregroundColor(Color.red)
-                }
-            }.offset(y:-3)
+            Section(header: Text("ProgressBar")) {
+                Text("現在は、\(Int(currentValue) * 100 / Int(maxValue))% です。")
+                
+                GeometryReader { geometryReader in
+                    ZStack(alignment: .leading) {
+                        Capsule()
+                            .frame(height: 7)
+                            .foregroundColor(Color(UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)))
+                        
+                        Capsule()
+                            .frame(width: self.progress(value: Double(currentValue),
+                                                        maxValue: Double(maxValue),
+                                                        width: geometryReader.size.width),
+                                   height: 7)
+                            .foregroundColor(Color.red)
+                    }
+                }.offset(y:-3)
+            }
             
             // 青 静的
-            GeometryReader { geometryReader in
-                ZStack(alignment: .leading) {
-                    Capsule()
-                        .frame(height: 7)
-                        .foregroundColor(Color(UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)))
-
-                    Capsule()
-                        .frame(width: self.progress(value: Double(49),
-                                                    maxValue: Double(100),
-                                                    width: geometryReader.size.width),
-                               height: 7)
-                        .foregroundColor(Color.blue)
-                }
-            }.offset(y:-3)
+            Section(header: Text("ProgressBar2")) {
+                Text("現在は、49% です。")
+                GeometryReader { geometryReader in
+                    ZStack(alignment: .leading) {
+                        Capsule()
+                            .frame(height: 7)
+                            .foregroundColor(Color(UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)))
+                        
+                        Capsule()
+                            .frame(width: self.progress(value: Double(49),
+                                                        maxValue: Double(100),
+                                                        width: geometryReader.size.width),
+                                   height: 7)
+                            .foregroundColor(Color.blue)
+                    }
+                }.offset(y:-3)
+            }
             
-            ProgressView(value: 0.35)
-                .frame(height: 30)
+            Section(header: Text("ProgressBar3 ios14~")) {
+                ProgressView(value: 0.35)
+                    .frame(height: 30)
+            }
             
-            ProgressView(value: 0.35)
-                .frame(height: 30)
-                .progressViewStyle(CircularProgressViewStyle(tint: Color.blue))
+            Section(header: Text("ActiveIndicator")) {
+                ProgressView(value: 0.35)
+                    .frame(height: 30)
+                    .progressViewStyle(CircularProgressViewStyle(tint: Color.blue))
+            }
             
-            ProgressView(value: 0.35)
-                .scaleEffect(x: 2, y: 2, anchor: .center) // indicatorの大きさ
-                .frame(width: 30, height: 30)
-                .padding(24)
-                .background(Color.gray)
-                .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
-                .cornerRadius(30)
+            Section(header: Text("ActiveIndicator2")) {
+                ProgressView(value: 0.35)
+                    .scaleEffect(x: 2, y: 2, anchor: .center) // indicatorの大きさ
+                    .frame(width: 30, height: 30)
+                    .padding(24)
+                    .background(Color.gray)
+                    .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                    .cornerRadius(30)
+            }
         }
         .padding(50)
         .frame(maxWidth: .infinity)
