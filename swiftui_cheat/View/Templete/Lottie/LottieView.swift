fee88688f2cc28c33bd8f49275b840b2ab5b6821
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-09-03 22:02:21 +0900

    fix lottie

--- swiftui_cheat/View/Templete/Lottie/LottieView.swift
+++ swiftui_cheat/View/Templete/Lottie/LottieView.swift
@@ -9,14 +9,14 @@ import Lottie
 struct LottiePlacticeView: View {
     var body: some View {
         VStack(spacing: 30) {
-            LottieView(name: "favorite_star", loopMode: .loop)
+            LottieView(name: "favorite_star")
                 .frame(width: 150, height: 150)
 
-            LottieView(name: "successful", loopMode: .loop)
+            LottieView(name: "successful")
                 .frame(width: 150, height: 150)
             
             ZStack {
-                LottieView(name: "voice_wave", loopMode: .loop)
+                LottieView(name: "voice_wave")
                     .frame(width: 250, height: 250)
                 Image("animal_kuma")
             }
@@ -26,16 +26,18 @@ struct LottiePlacticeView: View {
 
 struct LottieView: UIViewRepresentable {
     var name: String
-    var loopMode: LottieLoopMode = .playOnce
+    var loopMode: LottieLoopMode = .loop
     
     var animationView = AnimationView()
     
     func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
         let view = UIView(frame: .zero)
         
-        animationView.animation = Animation.named(name)
+        animationView.animation = Animation.named(name, animationCache: LRUAnimationCache.sharedCache)
         animationView.contentMode = .scaleAspectFit
         animationView.loopMode = loopMode
+        animationView.backgroundBehavior = .pauseAndRestore
+        animationView.respectAnimationFrameRate = true
         animationView.play()
         
         animationView.translatesAutoresizingMaskIntoConstraints = false
