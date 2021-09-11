6ba63eab7103e83916622242cc0684a20021302e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-09-11 14:21:06 +0900

    add picker file

---
+++ swiftui_cheat/View/Templete/Picker/PickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
