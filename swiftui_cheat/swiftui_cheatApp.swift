33db0d9654dfa6f208c6263443899ab2acb7f071
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-11-24 15:49:30 +0900

    add  environmentObject(PartialSheetManager()) for PartialSheet

--- swiftui_cheat/swiftui_cheatApp.swift
+++ swiftui_cheat/swiftui_cheatApp.swift
@@ -1,11 +1,13 @@
 
 import SwiftUI
+import PartialSheet
 
 @main
 struct swiftui_cheatApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(PartialSheetManager())
         }
     }
 }
