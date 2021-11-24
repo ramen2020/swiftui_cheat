286cd939ed10c20f9958cb9c3e04f3351bb0b7ec
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-11-24 15:51:19 +0900

    add PartialSheetView

---
+++ swiftui_cheat/View/Templete/PartialSheet/PartialSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  PartialSheetView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct PartialSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PartialSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PartialSheetView()
+    }
+}
