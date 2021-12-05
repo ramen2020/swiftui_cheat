c7f2895c8866036e51f6c4bd14f261f838b943ad
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-05 11:57:41 +0900

    add menu

---
+++ swiftui_cheat/View/Templete/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
