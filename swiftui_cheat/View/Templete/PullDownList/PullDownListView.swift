d212b628469f61bd778ba964f3ecb3e7c3906e62
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-09 22:11:39 +0900

    ADD PullDownListView

---
+++ swiftui_cheat/View/Templete/PullDownList/PullDownListView.swift
@@ -0,0 +1,18 @@
+//
+//  PullDownListView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct PullDownListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PullDownListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PullDownListView()
+    }
+}
