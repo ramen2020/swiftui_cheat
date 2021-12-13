fe77ab4252282c60e78a41d8077b8d393fce4cec
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-13 23:58:33 +0900

    add CustomScrollView file for SwiftUI

---
+++ swiftui_cheat/View/Templete/CustomScrollView/CustomScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomScrollView
+//  swiftui_cheat
+//
+
+import SwiftUI
+
+struct CustomScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomScrollView()
+    }
+}
