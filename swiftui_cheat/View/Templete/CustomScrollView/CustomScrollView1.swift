340a199b4910d3b3c3ed3a6ed44db2aa04397d7b
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-14 00:13:04 +0900

    add custom scroll view1

--- swiftui_cheat/View/Templete/CustomScrollView/CustomScrollView1.swift
+++ swiftui_cheat/View/Templete/CustomScrollView/CustomScrollView1.swift
@@ -7,7 +7,19 @@ import SwiftUI
 
 struct CustomScrollView1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CustomScrollView {
+            ForEach(0...10, id: \.self) { index in
+                Card1().padding()
+            }
+        } onRefresh: { endReflesh in
+            print("API実行")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                print("API接続成功：status code 200")
+                endReflesh()
+            }
+            // 非同期処理の完了時に引数のクロージャ(done)を実行する
+//            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: done)
+        }
     }
 }
 
